831f82aac541f28b97a0758270e97d2356ae7ae8
Scott Duane <csyleung@Cheryls-MacBook-Air.local>
2017-01-02 14:52:11 -0800

    Rails infrastructure

---
+++ db/migrate/20170102212221_create_note_table.rb
@@ -0,0 +1,10 @@
+class CreateNoteTable < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.string :title
+      t.string :content
+      t.integer :user_id
+      t.timestamps
+    end
+  end
+end
