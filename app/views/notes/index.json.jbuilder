98b8a52a51a7573e9fcc26bb3c9554dfc79cabd7
Scott Duane <csyleung@Cheryls-MacBook-Air.local>
2017-01-27 15:30:39 -0800

    Add Quill for note interface

---
+++ app/views/notes/index.json.jbuilder
@@ -0,0 +1,3 @@
+json.notes @notes do |note|
+  json.content = note.content
+end
--- frontend/components/AddNote.jsx
+++ frontend/components/AddNote.jsx
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import ReactQuill from 'react-quill';
+import { createNote } from '../util/note_api_util';
 
 class AddNote extends Component {
+  constructor () {
+    super();
+    this.content = "";
+  };
+
+  changeContent (input) {
+    this.content = input;
+  };
+
+  saveNote () {
+    createNote(input);
+    this.parent.toggleAdd();
+  };
+
   render () {
-    return <div>Add a note sillypants</div>;
+    let that = this;
+    return <div>
+      <h4>What do you want to say?</h4>
+      <ReactQuill  theme="snow"
+                   value={that.content}
+                   onChange={this.changeContent.bind(this)}
+                   placeholder="Type your note here..."
+                   className="note-content-input"/>
+                 <button className="save-button" onClick={that.saveNote}>Save Note</button>
+    </div>;
   };
 };
 
