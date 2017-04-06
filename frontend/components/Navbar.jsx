025a50d4097d0362c3f24fa1726f8a7a886c31bf
Scott Duane <adrian.scott.duane@gmail.com>
2017-04-06 10:37:15 -0700

    Test s3 logo link

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -7,7 +7,7 @@ class Navbar extends Component {
       <div className="navbar-wrapper">
         <ul className="navbar-class">
           <li><a href="http://www.linkedin.com/in/sduane"><img className="icon" src="public/images/linkedin-logo.svg" /></a></li>
-          <li><a href="http://www.github.com/ScottDuane"><img className="icon" src="assets/github.svg" /></a></li>
+          <li><a href="http://www.github.com/ScottDuane"><img className="icon" src="https://s3-us-west-1.amazonaws.com/elasticbeanstalk-us-west-1-218768822416/assets/github.svg" /></a></li>
           <li><a href="mailto:adrian.scott.duane@gmail.com"><img className="icon" src="assets/mail-black-envelope-symbol.svg" /></a></li>
         </ul>
       </div>);
