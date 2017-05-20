3bee150e5aa6c8e1b0c9d4d8e25439d6c46071b6
Scott Duane <adrian.scott.duane@gmail.com>
2017-05-20 12:21:48 -0700

    Add images to asset pipeline

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# 
-# require File.join(Rails.root,'lib','openshift_secret_generator.rb')
-#
-# RailsApp::Application.config.secret_token = initialize_secret(
-#  :token,
-#  '335a4e365ef2daeea969640d74e18f0e3cd9fae1abd8f4125691a880774ea6d456a29c0831aa6921bf86a710fe555e916f0673f5657619ec9df22e0409bec345'
-# )
--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -6,9 +6,9 @@ class Navbar extends Component {
     return (
       <div className="navbar-wrapper">
         <ul className="navbar-class">
-          <li><a href="http://www.linkedin.com/in/sduane"><img className="icon" src="https://s3-us-west-1.amazonaws.com/scottduanerails/assets/linkedin-logo.svg" /></a></li>
-          <li><a href="http://www.github.com/ScottDuane"><img className="icon" src="https://s3-us-west-1.amazonaws.com/elasticbeanstalk-us-west-1-218768822416/assets/github.svg" /></a></li>
-          <li><a href="mailto:adrian.scott.duane@gmail.com"><img className="icon" src="https://s3-us-west-1.amazonaws.com/scottduanerails/assets/mail-black-envelope-symbol.svg" /></a></li>
+          <li><a href="http://www.linkedin.com/in/sduane"><img className="icon" src="images/linkedin-logo.svg" /></a></li>
+          <li><a href="http://www.github.com/ScottDuane"><img className="icon" src="images/github.svg" /></a></li>
+          <li><a href="mailto:adrian.scott.duane@gmail.com"><img className="icon" src="images/mail-black-envelope-symbol.svg" /></a></li>
         </ul>
       </div>);
   };
